d3a8cc77517e99b5744735e7cc17ad60974efa8e
longdang <windy341995@outlook.com>
2019-12-08 23:36:29 +0700

    update UI

--- src/client/app/component/admin-layout/admin-layout.jsx
+++ src/client/app/component/admin-layout/admin-layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import {AppFooter} from "../app-layout/app-footer/app-footer";
 import {Icon, Menu} from "antd";
+import {LeftSideMenuBar} from "./left-side-menu-bar";
 export class AdminLayout extends React.Component {
     constructor(props) {
         super(props);
@@ -14,7 +15,6 @@ export class AdminLayout extends React.Component {
         const {showMenu =true } =this.props ;
         return(
             <div className='admin-layout'>
-                <LeftSideMenuBar />
                 <div className="header">
                     <div className="main-container">
                         <Link to='/'>
@@ -27,6 +27,7 @@ export class AdminLayout extends React.Component {
                 </div>
 
                 <div className="main-container main-center">
+                    <LeftSideMenuBar/>
                     {this.props.children}
                 </div>
                 <AppFooter/>
@@ -35,75 +36,3 @@ export class AdminLayout extends React.Component {
     }
 }
 
-class LeftSideMenuBar  extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-    render() {
-        return(
-            <div className='left-side-menu-bar'>
-                <Menu
-                    style={{ width: 256 }}
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
-                    mode={this.state.mode}
-                    onClick={({ item, key, keyPath, domEvent })=>{
-                        console.log(item)
-                    }}
-                    theme={this.state.theme}
-                >
-                    <Menu.Item
-                        key="1">
-                        <Link to='/dashboard'>
-                            Dashboard
-                        </Link>
-
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Link to='/add-product'>
-                            Thêm sản phẩm mới
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key="3">
-                        <Link to='/products'>
-                           Quản lý sản phẩm
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key="4">
-                        <Link to='/trials'>
-                            Danh Sách Dùng Thử
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="5">
-                        <Link to='/payments'>
-                            Hóa Đơn
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key="6">
-                        <Link to='/warehousing'>
-                            Nhập Kho
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key="7">
-                        <Link to='/update-banners'>
-                            Update ảnh bìa
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="8">
-                        <Link to='/edit-banners'>
-                            Quản lý ảnh bìa
-                        </Link>
-                    </Menu.Item>
-                </Menu>
-            </div>
-        )
-    }
-}
